23d4c3f107640fc4f0007db6ef641e4e666f7b26
azarrias <adolfo.zarrias@gmail.com>
2020-04-20 12:48:02 +0200

    Create renderer, clear and display screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL could not initialize.\n";
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << "\n";
+        std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
         return EXIT_FAILURE;
     }
 
@@ -12,21 +12,38 @@ int main() {
     constexpr unsigned short int screen_height{960};
     bool running = true;
 
+    // Create window
     SDL_Window* window = SDL_CreateWindow("Park-Man", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
                             screen_width, screen_height, SDL_WINDOW_SHOWN);
-
     if (nullptr == window) {
         std::cerr << "Window could not be created.\n";
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << "\n";
+        std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (nullptr == renderer) {
+        std::cerr << "Renderer could not be created.\n";
+        std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+        return EXIT_FAILURE;
     }
 
     while(running) {
+        // Handle input
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             if (SDL_QUIT == e.type) {
                 running = false;
             }
         }
+
+        // Clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyWindow(window);
